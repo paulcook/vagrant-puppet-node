5f7adaff8476ecc7292c64c099e3ffefa195aa1a
Paul Cook <paul.cook@alternateversion.com>
2015-01-22 09:31:00 -0500

    removed uneeded modules for node

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,5 +1,5 @@
-$rails_project = 'buckeye_vault'
-$db_names = ['buckeye_vault_development','buckeye_vault_test']
+$project_dir = 'mass_matrix'
+$db_names = ['mass_matrix_development','mass_matrix_test']
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
@@ -42,55 +42,22 @@ class { 'rvm':
 rvm::system_user { vagrant: }
 
 rvm_system_ruby {
-  'ruby-2.1.4':
+  'ruby-2.1.5':
     ensure => present,
     require => Class['rvm::system'],
     default_use => true;
 }
 
 rvm_gem {
-  'ruby-2.1.4/bundler':
+  'ruby-2.1.5/bundler':
     ensure => latest,
-    require => Rvm_system_ruby['ruby-2.1.4'];
+    require => Rvm_system_ruby['ruby-2.1.5'];
 }
 
 class {
   'rvm::passenger::apache':
-    version => '4.0.53',
-    ruby_version => 'ruby-2.1.4';
+    version => '4.0.57',
+    ruby_version => 'ruby-2.1.5';
 }
 
-# class { 'apache_passenger_vhost':
-#   require => Class['install_rvm_passenger'],
-# }
-
-# # Install Postgresql and setup databases
-class install_postgres {
-
-  class { 'postgresql::server': }
-
-  postgresql::server::db { $db_names:
-    user => 'rails',
-    password => 'tempPass4u'
-  }
-
-  package { 'libpq-dev':
-    ensure => installed
-  }
-
-  package { 'postgresql-contrib':
-    ensure  => installed,
-    require => Class['postgresql::server'],
-  }
-}
-
-class { 'install_postgres': }
-
-# exec {
-#   'bundle rails':
-#     command => 'bundle',
-#     cwd => "/vagrant/${rails_project}",
-#     user => 'vagrant',
-#     environment => ["HOME=/home/vagrant"],
-# }
-
+include '::mongodb::server'
